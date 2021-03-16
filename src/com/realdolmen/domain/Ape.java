aee4ad1d29227953d0838774699341cf9fd02aca
Youri <yourivanderleyden@gmail.Com>
2021-03-16 11:26:41 +0100

    New animal added

---
+++ src/com/realdolmen/domain/Ape.java
@@ -0,0 +1,12 @@
+package com.realdolmen.domain;
+
+public class Ape extends Animal{
+    public Ape(String name, int age) {
+        super(name, age);
+    }
+
+    public Ape() {
+        super();
+    }
+}
+
