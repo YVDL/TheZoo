8b816ffb7f6ae2507a8864efbc277935cb6a8ef3
Tom Van Laeken <tototonique@gmail.com>
2021-03-16 09:36:45 +0100

    Added showInitialOptions

--- src/com/realdolmen/Main.java
+++ src/com/realdolmen/Main.java
@@ -1,8 +1,33 @@
 package com.realdolmen;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Welcome to my humble world of programming");
+        showInitialOptions();
+    }
+
+    private static void showInitialOptions() {
+        System.out.println("Please select an option :");
+        System.out.println("1. Add animal");
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                addAnAnimal();
+                break;
+            default:
+                System.out.println("Type in a valid input!");
+                showInitialOptions();
+                break;
+        }
+
+    }
+
+    private static void addAnAnimal() {
+
     }
 }
